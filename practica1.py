ec179f4e62302314bfd0c6f09278109a1372e1e7
A-malagon <alejanm09@gmail.com>
2015-03-15 18:33:43 +0100

    practica1 Finalizada

--- practica1.py
+++ practica1.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import webapp
-import urllib
-import socket
 
 
-class acortaApp(webapp.webApp):
-    CortaEncontrada = -1
-    urlReal = {}
-    urlCorta = {}
+class AcortadorApp(webapp.webApp):
+
+    def __init__(self, hostname, port):
+        self.name = hostname
+        self.port = str(port)
+        self.urlReal = {}
+        self.urlCorta = {}
+        self.cortaEncontrada = 0
+        webapp.webApp.__init__(self, hostname, port)
+
+    def acortarURL(self, url):
+        if url.find("%3A%2F%2F") == -1:
+            url = "http://" + url
+        else:
+            url = url.split("%3A%2F%2F")[1]
+            url = "http://" + url
+        return url
 
     def pedirFormulario(self):
         formulario = '<form action="" method="POST" accept-charset="UTF-8">' +\
@@ -16,19 +28,12 @@ class acortaApp(webapp.webApp):
                      '<input type="submit" value="Enviar"></form>'
         return formulario
 
-    def acortarURL(self, cuerpo):
-        if cuerpo.find("http") == -1:
-            cuerpo = "http://" + cuerpo
-        else:
-            cuerpo = cuerpo.replace('%3A%2F%2F', '://')
-        return cuerpo
-
     def cuerpoMetodo(self, metodo, request):
         if metodo == "GET":
             cuerpo = ""
         elif metodo == "POST":
-            cuerpo = request.split('\r\n\r\n', 1)[1].split('=')[1]
-            cuerpo = cuerpo.replace('+', '')
+            cuerpo = request.split('\r\n\r\n', 1)[-1]
+            cuerpo = cuerpo.split('=')[1]
         return cuerpo
 
     def parse(self, request):
@@ -36,9 +41,6 @@ class acortaApp(webapp.webApp):
         metodo = request.split(" ")[0]
         recurso = request.split(" ")[1]
         cuerpo = self.cuerpoMetodo(metodo, request)
-        print metodo
-        print recurso
-        print cuerpo
         return (metodo, recurso, cuerpo)
 
     def process(self, parsedRequest):
@@ -52,45 +54,55 @@ class acortaApp(webapp.webApp):
                 htmlBody = ("<html><body>" + str(self.urlReal) +
                             formulario + "</body></html>")
             else:
-                recurso = int(recurso.split('/')[1])
+                recurso = recurso.split('/')[1]
                 if recurso in self.urlCorta:
-                    httpCode = ("300 Redirect\nLocation: " +
-                                self.urlCorta[recurso])
-                    htmlBody = "<html><body>Redireccion</body></html>"
+                    url = self.urlCorta[recurso]
+                    print "Redireccionando a la URL " + url
+                    httpCode = "300 Redirect"
+                    htmlBody = "<html><body><meta http-equiv='refresh'\
+                                content='0; \
+                                URL=" + url + "'></body></html>"
                 else:
                     httpCode = "404 Not Found"
-                    htmlBody = ("<html><body> Error:Recurso no disponible" +
-                                "</body></html>")
-
+                    htmlBody = "<html><body><h1>Recurso no disponible</h1>\
+                                </body></html>"
+            return httpCode, htmlBody
         elif metodo == "POST":
-
-            if cuerpo == "":
-                httpCode = "404 Not Found"
-                htmlBody = ("<html><body>Error</body></html>")
-                return (httpCode, htmlBody)
+            url = cuerpo
+            if url == "":
+                httpCode = "400 Error"
+                htmlBody = "<html><body><h1>Error:Post vacio</h1>\
+                            </body></html>"
+                return httpCode, htmlBody
+            url = self.acortarURL(url)
+            print "URL:" + url
+            if url in self.urlReal:
+                cortaEncontrada = self.urlReal[url]
             else:
-                cuerpo = self.acortarURL(cuerpo)
-
-            if cuerpo in self.urlReal:
-                self.CortaEncontrada = self.urlReal[cuerpo]
-            else:
-                self.CortaEncontrada = self.CortaEncontrada + 1
-                self.urlReal[cuerpo] = self.CortaEncontrada
-                self.urlCorta[self.CortaEncontrada] = cuerpo
-                print self.CortaEncontrada
-
+                cortaEncontrada = "http://" + self.name + ":" + \
+                                  self.port + "/" + \
+                                  str(self.cortaEncontrada)
+                self.urlReal[url] = cortaEncontrada
+                self.urlCorta[str(self.cortaEncontrada)] = url
+                self.cortaEncontrada += 1
             httpCode = "200 OK"
-            larga = "<a href ='" + cuerpo + "'> Url larga: " + cuerpo + "</a>"
-            corta = ("<a href ='http://" + str(socket.gethostname()) +
-                     ":1234/" + str(self.CortaEncontrada) +
-                     "'> Url acortada : " + str(self.CortaEncontrada) + "</a>")
-            htmlBody = ("<html><body> URLS: </br>" + larga + "</br>" +
-                        corta + "</br>" + "</body></html>")
-        return (httpCode, htmlBody)
+            htmlBody = "<html><body><p>url real: <a href=" +\
+                       url + ">" + url + \
+                       "</a></p>url acortada: <a href=" +\
+                       cortaEncontrada + ">" + cortaEncontrada +\
+                       "<body text='red'>" +\
+                       "<body bgcolor='#000000'>" +\
+                       "</a></body></html>"
+            return httpCode, htmlBody
+        else:
+            httpCode = "400 Error"
+            htmlBody = "<html><body><h1>Ni POST ni GET</h1></body></html>"
+        return httpCode, htmlBody
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        testacortador = acortaApp(socket.gethostname(), 1234)
+        Servidor = AcortadorApp("localhost", 1234)
     except KeyboardInterrupt:
         print "\nCerrando el servidor\n"
+    except TypeError:
+        print "host name[string]  port[int]"
